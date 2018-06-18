18403efc99866ddee310a11bde95fccb7bca1f68
stellaCmiller <stella.miller@utexas.edu>
2018-06-18 18:36:42 -0500

    win and lose counter functional

--- assets/logic.js
+++ assets/logic.js
@@ -1,44 +1,53 @@
+
+//Game related variables-----------------------------------------------------------------------------
 var alphabet = "abcdefghijklmnopqrstuvwxyz";
 var wordBank = ["succulent", "prickly pear", "dehydration", "oasis", "rattlesnake", "mirage", "pyramid", "outlaw", "gallows", "tumbleweeds", "sand dunes", "camel", "tarantula"]
-
-
-var secretWord = randomWord(wordBank);
 var secretWordCharArray = [];
 var guessedLetters = [];
-var numGuesses = 5;
+var numGuesses = 5
+var winCounter = 0;
+var loseCounter = 0;
 
-
+var secretWord = randomWord(wordBank);
 createGameSpace(secretWord);
-console.log(secretWord);
+console.log(secretWord);//test function, remove later
+
+
+function reset(){
+
+}
+
+document.onkeyup = function (event) {
+    if (numGuesses > 0) {
+        var userGuess = event.key;
+
+        if (alphabet.includes(userGuess)) {
+            checkGuess(userGuess);
+        }
+        else {
+            alert("That is not a letter!")
+        }
+    }
+}
+
 
 //Returns a random word from the wordbank array passed into it
-function randomWord(bank){
+function randomWord(bank) {
     randomIndex = Math.floor(Math.random() * Math.floor(bank.length));
     return bank[randomIndex];
 }
 
 /*Takes the secret word, creates an underscore array representation of the word, and then prints it to the screen*/
 function createGameSpace(secretWord) {
-    for (let i=0; i<secretWord.length; i++){
-        if (secretWord.charAt(i) === " "){
+    for (let i = 0; i < secretWord.length; i++) {
+        if (secretWord.charAt(i) === " ") {
             secretWordCharArray.push("\xa0");
         } else {
             secretWordCharArray.push("_");
         }
-    }   
-    for (let character in secretWordCharArray){
+    }
+    for (let character in secretWordCharArray) {
         document.getElementById("gameSpace").textContent += secretWordCharArray[character] + "\xa0";
-    }
-}
-
-document.onkeyup = function(event){
-    var userGuess = event.key;
-
-    if (alphabet.includes(userGuess)){
-        checkGuess(userGuess); 
-    }
-    else {
-        alert("That is not a letter!")
     }
 }
 
@@ -46,29 +55,48 @@ document.onkeyup = function(event){
 function checkGuess(guess) {
     if (guessedLetters.includes(guess)) {
         alert("You have already guessed that letter");
-    } else if (secretWord.includes(guess)){
+    } else if (secretWord.includes(guess)) {
         replaceUnderscores(guess);
         guessedLetters.push(guess);
         document.getElementById("guessedLetters").textContent += guess;
     } else {
         alert("lol you suck");
-        numGuesses --;
+        numGuesses--;
         document.getElementById("lives").textContent = numGuesses;
         guessedLetters.push(guess);
         document.getElementById("guessedLetters").textContent += guess;
     }
+    checkWinCondition();
 }
 
 /*Updates the secret word character array and the underscore display whenever a user correctly guesses a letter */
-function replaceUnderscores(character){
-    for (i=0; i<secretWord.length; i++){
-        if (secretWord.charAt(i) == character){
+function replaceUnderscores(character) {
+    for (i = 0; i < secretWord.length; i++) {
+        if (secretWord.charAt(i) == character) {
             secretWordCharArray[i] = character;
         }
-    } 
+    }
     let updatedGameSpace = "";
-    for (let character in secretWordCharArray){
+    for (let character in secretWordCharArray) {
         updatedGameSpace += secretWordCharArray[character] + "\xa0";
         document.getElementById("gameSpace").textContent = updatedGameSpace;
     }
 }
+
+/*After each guess, this functions checks if the player has won or lost and performs actions accordingly */
+function checkWinCondition() {
+    if (numGuesses == 0) {
+        alert("You Lost!");
+        loseCounter++;
+        document.getElementById("losses").textContent = loseCounter;
+        reset();
+    } else if (!secretWordCharArray.includes("_")) {
+        setTimeout(function () { alert("Congrats, you win!!") }, 300);//Makes sure the alert appears after the visual changes
+        winCounter++;
+        document.getElementById("wins").textContent = winCounter;
+        reset();
+    }
+}
+
+
+
--- assets/logic2electricboogaloo.js
+++ assets/logic2electricboogaloo.js
@@ -6,7 +6,14 @@ gaem = {
     alphabet: "abcdefghijklmnopqrstuvwxyz",
     guessedLetters: [],
     secretWord: "",
-    secretWordCharArray: []
+    secretWordCharArray: [],
+    winCounter: 0,
+    loseCounter: 0,
+    getRandomWord: function randomWord(bank) {
+        randomIndex = Math.floor(Math.random() * Math.floor(bank.length));
+        return bank[randomIndex];
+    }
+
 }
 
 MyGame = new gaem;
