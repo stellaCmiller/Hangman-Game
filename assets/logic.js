4fbb046cb094ebcc331acf599760d0efc64317e1
stellaCmiller <stella.miller@utexas.edu>
2018-06-17 20:56:16 -0500

    continued on logic, added a placeholder background image

--- assets/logic.js
+++ assets/logic.js
@@ -3,6 +3,11 @@ var wordBank = ["succulent", "prickly pear", "dehydration", "oasis", "rattlesnak
 
 
 var secretWord = randomWord(wordBank);
+var secretUnderscores = "";
+var guessedLetters = [];
+var numGuesses = 5;
+
+
 createGameSpace(secretWord);
 console.log(secretWord);
 
@@ -12,13 +17,15 @@ function randomWord(bank){
     return bank[randomIndex];
 }
 
-//Takes the secret word and represents it with underscores on the HTML page
+/*Takes the secret word and represents it with underscores on the HTML page and also adds it to a String to be iterated over later in order to replace characters*/
 function createGameSpace(secretWord) {
     for (i=0; i<secretWord.length; i++){
-        if (secretWord.charAt(i) === " "){
+        if (secretWord.charAt(i) === "\xa0"){
+            secretUnderscores += "\xa0\xa0";
             document.getElementById("gameSpace").textContent += "\xa0\xa0";
         } else {
-        document.getElementById("gameSpace").textContent += "_ ";
+            secretUnderscores += "_";
+            document.getElementById("gameSpace").textContent += "_ ";
         }
     }
 }
@@ -27,15 +34,34 @@ document.onkeyup = function(event){
     var userGuess = event.key;
 
     if (alphabet.includes(userGuess)){
-        checkGuess() 
+        checkGuess(userGuess); 
     }
     else {
         alert("That is not a letter!")
     }
 }
 
+/*Checks whether the user has already guessed a letter, and if they have, checks whether the secret word contains the key pressed by the user and either reveals it on the page or decreases the number of lives accordingly, adding the character to the list of guesses already attempted.*/
 function checkGuess(guess) {
-    if (userGuess.includes(secretWord)){
-        dosomething();
+    if (guessedLetters.includes(guess)) {
+        alert("You have already guessed that letter");
+    } else if (secretWord.includes(guess)){
+        replaceUnderscores(guess);
+    } else {
+        alert("lol you suck");
+        numGuesses --;
+        document.getElementById("lives").textContent = numGuesses;
+        guessedLetters.push(guess);
+        document.getElementById("guessedLetters").textContent += guess;
     }
+}
+
+function replaceUnderscores(character){
+    for (i=0; i<secretWord.length; i++){
+        if (secretWord.charAt(i) == character){
+            secretUnderscores.charAt(i) = character;
+        }
+    }
+    document.getElementById("gameSpace").textContent = secretUnderscores;
+    console.log(secretUnderscores);
 }
